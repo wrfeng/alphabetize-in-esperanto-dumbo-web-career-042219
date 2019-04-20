a8577e9da904e8f754a5dc1b8b3aa3cab1204c6c
wei feng <theweifeng@gmail.com>
2019-04-20 19:18:58 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,9 @@
 def alphabetize(arr)
-  # code here
+  alpha = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  
+  arr.sort_by do |sentence|
+    sentence.split("").map do |character|
+      alpha.index(character)
+    end
+  end
 end
