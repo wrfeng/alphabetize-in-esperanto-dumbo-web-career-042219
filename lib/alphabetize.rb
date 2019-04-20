57aaaaeb10a3dc5ac0b55bcc25f6210a325cadbe
wei feng <theweifeng@gmail.com>
2019-04-20 19:17:06 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,5 +1,9 @@
 def alphabetize(arr)
   ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   
-  arr.sort_by {password_field_tag}
+    sentences_array.sort_by do |sentence|
+    sentence.split("").map do |character|
+      ESPERANTO_ALPHABET.index(character)
+    end
+  end
 end
