eb92c5ea4283e337c94eee986be49279b8a99fee
wei feng <theweifeng@gmail.com>
2019-04-20 19:18:35 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,9 +1,9 @@
 def alphabetize(arr)
-  ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  alpha = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   
   arr.sort_by do |sentence|
     sentence.split("").map do |character|
-      ESPERANTO_ALPHABET.index(character)
+      alpha.index(character)
     end
   end
 end
