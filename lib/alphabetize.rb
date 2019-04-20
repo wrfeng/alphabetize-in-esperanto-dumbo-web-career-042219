a32e19a0ee035486a3531197bf380c127626f4e2
wei feng <theweifeng@gmail.com>
2019-04-20 19:17:54 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,7 +1,7 @@
 def alphabetize(arr)
   ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   
-    sentences_array.sort_by do |sentence|
+  arr.sort_by do |sentence|
     sentence.split("").map do |character|
       ESPERANTO_ALPHABET.index(character)
     end
